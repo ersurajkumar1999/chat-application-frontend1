0204567fec47fd94a7f925fbd07e9076df510b30
ersurajkumar1999 <er.surajkumar1999@gmai.com>
2023-12-17 00:23:30 +0530

    Home page added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
+import Home from './Home';
 
 function App() {
   return (
-    <div className="App">
-      Home Page
-    </div>
+    <Home />
   );
 }
 
